2dfb093ed40a07ea8e436c3cab6c690425fd0a3d
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-04-09 12:05:12 +0200

    Adds task 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ import {
   ForbesList,
   CryptoHistory,
 } from 'components';
+import forbesData from 'data/forbes.json';
 
 export const App = () => {
   return (
@@ -35,7 +36,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
         </Heading>
-        <ForbesList />
+        <ForbesList list={forbesData} />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
