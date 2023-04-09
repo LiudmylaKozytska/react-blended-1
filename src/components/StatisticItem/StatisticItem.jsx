971675018084326c0b790756e710db1bb3ba0345
annabusaga <anna.busaga@gmail.com>
2023-04-09 12:18:35 +0300

    commit

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -1,9 +1,16 @@
+import { IconContext } from 'react-icons';
 import {
   StatisticBox,
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({ total, icon, title }) => {
+  return (
+    <StatisticBox>
+      <IconContext.Provider value={{ size: 24 }}>{icon}</IconContext.Provider>
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,26 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map((stat, index) => (
+          <StatisticItem
+            key={stat.id}
+            total={stat.total}
+            title={stat.title}
+            icon={icons[index]}
+          />
+        ))}
+      </StatisticsList>
     </>
   );
 };
